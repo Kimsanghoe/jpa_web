00b8e0dd1bce8cf2d9a9ac18f9e73a5a2590ec13
Kim sang hoe <mace302@naver.com>
2023-05-21 15:56:18 +0900

    20230521_도메인 분석 설계

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,12 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {} // JAP 스펙상 생성해 둔 것 사용X
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
